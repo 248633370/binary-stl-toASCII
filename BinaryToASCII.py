fe51cce369848c5edaf96443196c75979542dd21
NAR <nar@necheta.ru>
2017-11-06 04:03:26 +0300

    add filename as option

--- BinaryToASCII.py
+++ BinaryToASCII.py
@@ -1,50 +1,61 @@
+#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 import struct
+import sys
 
-infile = open('binary.stl') #import file
-out = open('ASCII.stl', 'w') #export file
-
-data = infile.read()
-
-
-out.write("solid ")
-
-for x in xrange(0,80):
-    if not ord(data[x]) == 0:
-        out.write(struct.unpack('c', data[x])[0])
+def main():
+    if len(sys.argv) < 2:
+        print("Need filename as 1st option")
+        sys.exit(1)
     else:
-        pass
-out.write("\n")
+        inputfile = sys.argv[1] 
+    infile = open(inputfile) #import file
+    out = open(inputfile + 'ASCII.stl', 'w') #export file
+    
+    data = infile.read()
 
-number = data[80] + data[81] + data[82] + data[83] 
-faces = struct.unpack('I',number)[0]
+    out.write("solid ")
 
-for x in range(0,faces):
-    out.write("facet normal ")
+    for x in xrange(0,80):
+        if not ord(data[x]) == 0:
+            out.write(struct.unpack('c', data[x])[0])
+        else:
+            pass
+    out.write("\n")
 
-    xc = data[84+x*50] + data[85+x*50] + data[86+x*50] + data[87+x*50]
-    yc = data[88+x*50] + data[89+x*50] + data[90+x*50] + data[91+x*50]
-    zc = data[92+x*50] + data[93+x*50] + data[94+x*50] + data[95+x*50]
+    number = data[80] + data[81] + data[82] + data[83] 
+    faces = struct.unpack('I',number)[0]
 
-    out.write(str(struct.unpack('f',xc)[0]) + " ")
-    out.write(str(struct.unpack('f',yc)[0]) + " ")
-    out.write(str(struct.unpack('f',zc)[0]) + "\n")
+    for x in range(0,faces):
+        out.write("facet normal ")
 
-    out.write("outer loop\n")
-
-    for y in range(1,4):
-        out.write("vertex ")
-
-        xc = data[84+y*12+x*50] + data[85+y*12+x*50] + data[86+y*12+x*50] + data[87+y*12+x*50]
-        yc = data[88+y*12+x*50] + data[89+y*12+x*50] + data[90+y*12+x*50] + data[91+y*12+x*50]
-        zc = data[92+y*12+x*50] + data[93+y*12+x*50] + data[94+y*12+x*50] + data[95+y*12+x*50]
+        xc = data[84+x*50] + data[85+x*50] + data[86+x*50] + data[87+x*50]
+        yc = data[88+x*50] + data[89+x*50] + data[90+x*50] + data[91+x*50]
+        zc = data[92+x*50] + data[93+x*50] + data[94+x*50] + data[95+x*50]
 
         out.write(str(struct.unpack('f',xc)[0]) + " ")
         out.write(str(struct.unpack('f',yc)[0]) + " ")
         out.write(str(struct.unpack('f',zc)[0]) + "\n")
 
-    out.write("endloop\n")
-    out.write("endfacet\n")
+        out.write("outer loop\n")
 
-out.close()
-print "end"
+        for y in range(1,4):
+            out.write("vertex ")
+
+            xc = data[84+y*12+x*50] + data[85+y*12+x*50] + data[86+y*12+x*50] + data[87+y*12+x*50]
+            yc = data[88+y*12+x*50] + data[89+y*12+x*50] + data[90+y*12+x*50] + data[91+y*12+x*50]
+            zc = data[92+y*12+x*50] + data[93+y*12+x*50] + data[94+y*12+x*50] + data[95+y*12+x*50]
+
+            out.write(str(struct.unpack('f',xc)[0]) + " ")
+            out.write(str(struct.unpack('f',yc)[0]) + " ")
+            out.write(str(struct.unpack('f',zc)[0]) + "\n")
+
+        out.write("endloop\n")
+        out.write("endfacet\n")
+
+    out.close()
+    print "end"
+    
+if __name__ == "__main__":
+    main()
+
